257a3e9191084d635fc44f6cb5e83ccefd313cf6
bradserlis <bradserlis@yahoo.com>
2018-03-21 11:14:03 -0700

    Commit 5 - Getting to Know You answered

--- wednesday/answers.js
+++ wednesday/answers.js
@@ -48,28 +48,39 @@
 // for(var i=1; i <101; i++){
 // 	console.log(i);
 // }
-for(var i=1; i <101; i++){
-	if(i%3 === 0){
-		console.log("Fizz")
-	}else{
-		console.log(i);
-	}
-}
 
-for(var i=1; i <101; i++){
-	if(i%5 === 0){
-		console.log("Buzz")
-	}else{
-		console.log(i);
-	}
-}
+// for(var i=1; i <101; i++){
+// 	if(i%3 === 0){
+// 		console.log("Fizz")
+// 	}else{
+// 		console.log(i);
+// 	}
+// }
 
-for(var i=1; i <101; i++){
-	if(i%3 === 0 && i%5 === 0){
-		console.log("FizzBuzz")
-	}else{
-		console.log(i);
-	}
-}
+// for(var i=1; i <101; i++){
+// 	if(i%5 === 0){
+// 		console.log("Buzz")
+// 	}else{
+// 		console.log(i);
+// 	}
+// }
 
+// for(var i=1; i <101; i++){
+// 	if(i%3 === 0 && i%5 === 0){
+// 		console.log("FizzBuzz")
+// 	}else{
+// 		console.log(i);
+// 	}
+// }
+
+//5
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom[0] = "Gameboy";
+karolin[1] = 17;
+matt[2] = "Gotham City";
+kristyn[2] = "Philadelphia";
 
