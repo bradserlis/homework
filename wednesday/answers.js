748d236d1e4cc8057fbc426901aeb861addd4e26
bradserlis <bradserlis@yahoo.com>
2018-03-21 17:14:13 -0700

    Part one of triangles

--- wednesday/answers.js
+++ wednesday/answers.js
@@ -157,10 +157,23 @@
 // thomInventory();
 
 // 9. 
-var sum = 0;
-for(var i=0; i<1000; i++){
-	if(i%3===0 || i%5===0){
-		sum += i;
-	}
-}
-console.log(sum);
+// var sum = 0;
+// for(var i=0; i<1000; i++){
+// 	if(i%3===0 || i%5===0){
+// 		sum += i;
+// 	}
+// }
+// console.log(sum);
+
+//Triangle
+
+// let triangle = '';
+// function triangleMaker(n){
+// 	for(var i=0; i<n; i++){
+// 		triangle += '#';
+// 		console.log(triangle);
+// 	}
+// }
+
+// triangleMaker(7);
+
